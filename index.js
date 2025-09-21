3f2b6f75cffa8ef02d79fc0fb1a59889bf0af81b
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2025-09-21 18:42:45 +0100

    Implement range-based video streaming; add `index.html` for video playback testing and accompanying `package-lock.json`

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import fs from 'fs'
 import http from 'http'
 import path from 'path'
 
-const server = http.createServer((req, res) => {
+
+const getFileStatistics = async (filePath)=> {
+    return fs.promises.stat(filePath)
+}
+
+const server = http.createServer(async (req, res) => {
     const videoPath = path.join('videos', 'Blade Runner 2049 ｜ Doja Cat - So High ｜ HDR10+ [bVUSYNg8qcY].mp4');
     console.log('fetching ' + videoPath)
 
@@ -11,15 +16,48 @@ const server = http.createServer((req, res) => {
         res.end('Video not found')
         return
     }
-    res.writeHead(206, { 'Content-Type': 'video/mp4' })
 
-    const stream = fs.createReadStream(videoPath)
-    stream.pipe(res)
+    const range = req.headers.range
+    let fileStatistics;
+    let fileSize;
+    try {
+       fileStatistics = await getFileStatistics(videoPath)
+        fileSize = fileStatistics.size
+    } catch (err) {
+        console.error('Error getting video size:', err)
+        res.end(err.message)
+    }
 
-    stream.on('error', (err) => {
-        console.error('Error streaming video:', err)
-        res.end(err)
-    })
+    if (range) {
+        const byteRange = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(byteRange[0], 10)
+        const end = byteRange[1] ? parseInt(byteRange[1], 10) : fileSize -1
+
+        if (start >=fileSize || end >= fileSize) {
+            res.writeHead(416, {'Content-range': `bytes */${fileSize}`})
+            res.end()
+        }
+
+        const chunkSize = (end -start) + 1
+        const file = fs.createReadStream(videoPath, {start, end})
+
+        res.writeHead(206,{
+            'content-range':`bytes ${start} - ${end}/${fileSize}`,
+            'accept-ranges':'bytes',
+            'content-length': chunkSize,
+            'content-type': 'video/mp4'
+        })
+
+        file.pipe(res)
+
+    } else {
+        res.writeHead(200, {
+            'content-length': fileSize,
+            'content-type': 'video/mp4'
+        })
+        const file = fs.createReadStream(videoPath)
+        file.pipe(res)
+    }
 })
 
 server.listen(3000, () => console.log('Server running on port 3000'))
